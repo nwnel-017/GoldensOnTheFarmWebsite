6668dfbf3230429ed2d50f56504ae2f9da82cbc3
nwnel-017 <nnelson19@ewu.edu>
2023-03-23 16:13:53 -0700

    sticky nav bar added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+const tabs = document.querySelectorAll('tabs')
 
 tabs.forEach(tab => {
+    tab.classList.remove('active-tab')
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
         })
         target.classList.add('active')
+        tabs.forEach(tab => {
+            tab.classList.remove('active-tab')
+        })
+        tab.classList.add('active-tab')
     })
 })

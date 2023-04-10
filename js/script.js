489d6f277b6977e5383365067fde5b362948b332
nwnel-017 <nnelson19@ewu.edu>
2023-04-10 15:11:24 -0700

    active tabs implemented

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-const tabs = document.querySelectorAll('tabs')
+// const tabs = document.getElementsByClassName('tabs')
 
-tabs.forEach(tab => {
-    tab.classList.remove('active-tab')
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
-        target.classList.add('active')
-        tabs.forEach(tab => {
-            tab.classList.remove('active-tab')
-        })
-        tab.classList.add('active-tab')
+// //cant do this-> need to find another way
+// tabs.forEach(tab => {
+//     console.log("Tab: " )
+//     tab.classList.remove('active-tab')
+//     tab.addEventListener('click', () => {
+//         tabs.forEach(tab => {
+//             tab.classList.remove('active-tab')
+//         })
+//         tab.classList.add('active-tab')
+//     })
+// })
+
+//returns a collection
+const tabs = document.getElementById("tabs").getElementsByTagName("a")
+
+for(var i = 0; i< tabs.length; i++) {
+    console.log(tabs[i])
+    tabs[i].addEventListener('click', function(event) {
+        for(var j = 0; j < tabs.length; j++) {
+            tabs[j].classList.remove('active-tab')
+        }
+        this.classList.add('active-tab')
     })
-})
+}
+
+window.onscroll = () => {
+    console.log("scrolling")
+}
+

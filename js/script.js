9af923644e56b219cf3d6bb23b68e660b5cd49e6
nwnel-017 <nnelson19@ewu.edu>
2023-04-14 15:20:39 -0700

    finished scrolling feature, logo added

--- js/script.js
+++ js/script.js
@@ -25,7 +25,19 @@ for(var i = 0; i< tabs.length; i++) {
     })
 }
 
-window.onscroll = () => {
-    console.log("scrolling")
-}
+const sections = document.querySelectorAll('section[id]')
+window.addEventListener('scroll', () => {
+    const scrollY = window.pageYOffset;
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 200;
+        const sectionId = current.getAttribute('id');
+        console.log(sectionId);
+        if(scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+            document.querySelector('a[href*="' + sectionId + '"]').classList.add('active-tab');
+        } else {
+            document.querySelector('a[href*="' + sectionId + '"]').classList.remove('active-tab');
+        }
+    })
+})
 

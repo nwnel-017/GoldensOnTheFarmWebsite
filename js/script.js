b64e187cc22811f3a5ead9cb8fe66dec252977a5
nwnel-017 <nnelson19@ewu.edu>
2023-05-22 12:48:10 -0700

    pics added

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
-// const tabs = document.getElementsByClassName('tabs')
-
-// //cant do this-> need to find another way
-// tabs.forEach(tab => {
-//     console.log("Tab: " )
-//     tab.classList.remove('active-tab')
-//     tab.addEventListener('click', () => {
-//         tabs.forEach(tab => {
-//             tab.classList.remove('active-tab')
-//         })
-//         tab.classList.add('active-tab')
-//     })
-// })
-
 //returns a collection
 const tabs = document.getElementById("tabs").getElementsByTagName("a")
 
 for(var i = 0; i< tabs.length; i++) {
-    console.log(tabs[i])
+    // console.log(tabs[i])
     tabs[i].addEventListener('click', function(event) {
         for(var j = 0; j < tabs.length; j++) {
             tabs[j].classList.remove('active-tab')
         }
         this.classList.add('active-tab')
+        // console.log(this)
     })
 }
 
@@ -32,7 +19,7 @@ window.addEventListener('scroll', () => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 200;
         const sectionId = current.getAttribute('id');
-        console.log(sectionId);
+        // console.log(sectionId);
         if(scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
             document.querySelector('a[href*="' + sectionId + '"]').classList.add('active-tab');
         } else {
